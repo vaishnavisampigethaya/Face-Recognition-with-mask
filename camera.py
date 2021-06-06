28f84d32bda126847c29157670307866b226d36a
VAISHNAVI <49392177+vaishnavisampigethaya@users.noreply.github.com>
2021-06-06 10:53:23 +0530

    Add files via upload

--- camera.py
+++ camera.py
@@ -16,12 +16,13 @@ class VideoCamera(object):
     def get_frame(self):
         image = self.stream.read()
         image = cv2.resize(image, (224, 224))
+        img=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         li = ['chaya', 'manja', 'queany', 'rakesh', 'sam', 'sankalpa', 'sapana', 'shradda', 'shreya', 'sowmya', 'suprabha', 'swathi', 'thrupthi', 'vaishali', 'vaishu']
-        haar = cv2.CascadeClassifier('haarcascade_eye.xml')
-        haar1 = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        eyes = haar.detectMultiScale(image)
-        face = haar1.detectMultiScale(image)
-        model=keras.models.load_model('facerecognition.h5')
+        haar = cv2.CascadeClassifier('C:\\haarcascades\\haarcascade_eye.xml')
+        haar1 = cv2.CascadeClassifier('C:\\haarcascades\\haarcascade_frontalface_default.xml')
+        eyes = haar.detectMultiScale(img)
+        face = haar1.detectMultiScale(img)
+        model=keras.models.load_model('C:\\Users\\User\\OneDrive\\Desktop\\FaceRecognitionFlask-master\\facerecognition.h5')
         y_pred = int(model.predict_classes(image.reshape(1, 224, 224, 3)))
         #y_pred1 = model1.predict_classes(image.reshape(1, 224, 224, 3))
         name = li[y_pred]
@@ -37,10 +38,9 @@ class VideoCamera(object):
             endX = int(right)
             endY = int(bottom)
             cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
-
             cv2.putText(image, name, (endX - 70, endY - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-
         data = []
-        data.append(image.tobytes())
+        ret,jpeg=cv2.imencode('.jpg',image)
+        data.append(jpeg.tobytes())
         data.append(name)
         return data
--- form_data.py
+++ form_data.py
@@ -43,6 +43,7 @@ def camera():
 def gen(camera):
     while True:
         data= camera.get_frame()
+
         frame=data[0]
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
@@ -54,22 +55,6 @@ def video_feed():
 
 @app.route('/result',methods=["POST","GET"])
 def Result():
-    if request.method=="POST":
-        print(list(request.form.values()))
-        result=list(request.form.values())[0]
-        if result.lower()=="restart":
-            output.clear()
-        else:
-            try:
-                r = requests.post('http://localhost:5000/webhooks/rest/webhook', json={"message": result})
-                print("Bot says, ")
-                for i in r.json():
-                    bot_message = i['text']
-                    print(f"{i['text']}")
-                output.extend([("message parker",result),("message stark",bot_message)])
-            except:
-                output.extend([("message parker", result), ("message stark", "We are unable to process your request at the moment. Please try again...")])
-
         print(output)
         return render_template("IY_Home_page.html",result=output)
 
